7836fed640bbb54af8d1c231176cb11d9e19589e
n-jing <siliuhe@sina.com>
2020-12-28 22:16:07 +0800

    fix bugs of dof, not depth test

--- shader/display.cpp
+++ shader/display.cpp
@@ -161,8 +161,14 @@ int main()
   glBindTexture(GL_TEXTURE_2D, 0);
 
   glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, sceneBuffer, 0);
-  
-// render loop
+  unsigned int rbo;
+  glGenRenderbuffers(1, &rbo);
+  glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT); // use a single renderbuffer object for both a depth AND stencil buffer.
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+
+
+  // render loop
   // -----------
   float lastFrame = 0.0f;
   while (!glfwWindowShouldClose(window))
